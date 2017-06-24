582ae6bbc2df342e7d3bd4fbc02e3035b78243d8
unknown <18710160187@163.com>
2017-06-24 17:27:53 +0800

    Initial commit

---
+++ Chy/src/cn/servlet/servlet.java
@@ -0,0 +1,16 @@
+package cn.servlet;
+
+import java.io.IOException;
+
+/**
+ * Created by 18710 on 2017/6/24.
+ */
+public class servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
